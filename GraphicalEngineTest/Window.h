8c15a2159d0513680708e3b9136b6eb7ac15391b
Josef Lintz <yosi1.lints@gmail.com>
2020-07-05 12:12:55 +0300

    + Added comments to Window class

--- GraphicalEngineTest/Window.h
+++ GraphicalEngineTest/Window.h
@@ -2,18 +2,50 @@
 #include <Windows.h>
 #include <string>
 
+
+/// <summary>
+/// A window class, encapsulates normal Windows window functionality into a single class
+/// </summary>
 class Window
 {
+
 private:
 
+    /// <summary>
+    /// A boolean flag that indicates if the window messages procedure is bound to the instanced function
+    /// </summary>
+    inline static bool winProcBound = false;
+
+private:
+
+    /// <summary>
+    /// Handle to the window
+    /// </summary>
     HWND _hwnd;
 
+    /// <summary>
+    /// Handle to process instnace
+    /// </summary>
     HINSTANCE _hInstance;
 
+    /// <summary>
+    /// A registered window class name
+    /// </summary>
     std::wstring _windowClassName;
+
+    /// <summary>
+    /// The tile of this window
+    /// </summary>
     std::wstring _windowTitle;
 
+    /// <summary>
+    /// Window width
+    /// </summary>
     int _windowWidth;
+
+    /// <summary>
+    /// Window height
+    /// </summary>
     int _windowHeight;
 
 public:
@@ -39,13 +71,19 @@ public:
 
 public:
 
-
+    /// <summary>
+    /// Shows the window after constructing it
+    /// </summary>
     void ShowWindow()
     {
         ::ShowWindow(_hwnd, SW_SHOW);
     };
 
-
+    /// <summary>
+    /// Processes messages in window message buffer. Returns true after all messages were handled, 
+    /// if WM_QUIT message was sent will return false
+    /// </summary>
+    /// <returns></returns>
     bool ProcessMessageBuffer()
     {
         // Windows message loop
@@ -80,11 +118,47 @@ public:
 
 private:
 
+
+    /// <summary>
+    /// A window message dispatcher, the only purpose of this function is to dispatch window messages to the instanced WinProc in this class
+    /// </summary>
+    /// <param name="hwnd"></param>
+    /// <param name="msg"></param>
+    /// <param name="wParam"></param>
+    /// <param name="lParam"></param>
+    /// <returns></returns>
     static LRESULT CALLBACK WinProcDispatch(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
     {
-        Window* windowPointer = reinterpret_cast<Window*>(GetWindowLongPtrW(hwnd, GWLP_USERDATA));
-        return windowPointer->WindowProcedure(hwnd, msg, wParam, lParam);
+        // If window procedure isn't bound
+        if (winProcBound == false)
+        {
+            // Check if window finished creation
+            if (msg == WM_NCCREATE)
+            {
+                // Get a pointer to the Window
+                const CREATESTRUCTW* windowDataPointer = reinterpret_cast<CREATESTRUCTW*>(lParam);
+                Window* windowPointer = reinterpret_cast<Window*>(windowDataPointer->lpCreateParams);
+
+                // Set GWLP_USERDATA to the window pointer so we can retrieve it later
+                SetWindowLongPtrW(hwnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(windowPointer));
+
+                winProcBound = true;
+
+                // Dispatch the message to the instanced window procedure
+                return windowPointer->WindowProcedure(hwnd, msg, wParam, lParam);
+            };
+
+            // If any other message is received prior, handle it with DefWindowProc
+            return DefWindowProcW(hwnd, msg, wParam, lParam);
+        }
+        else
+        {
+            // Get Window ptr from bound user data and dispatch window message to the Window's WindowProcedure
+            Window* windowPointer = reinterpret_cast<Window*>(GetWindowLongPtrW(hwnd, GWLP_USERDATA));
+            return windowPointer->WindowProcedure(hwnd, msg, wParam, lParam);
+        };
     };
+
 
     /// <summary>
     /// A window message handler for the main window
@@ -111,26 +185,11 @@ private:
 
 private:
 
+    /// <summary>
+    /// Creates the window handle
+    /// </summary>
     void CreateWindowHandle()
     {
-        auto tempWindowProc = [](HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) -> LRESULT
-        {
-            if (msg == WM_NCCREATE)
-            {
-                const CREATESTRUCTW* windowDataPointer = reinterpret_cast<CREATESTRUCTW*>(lParam);
-                Window* windowPointer = reinterpret_cast<Window*>(windowDataPointer->lpCreateParams);
-                
-                SetWindowLongPtrW(hwnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(windowPointer));
-
-                SetWindowLongPtrW(hwnd, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(Window::WinProcDispatch));
-
-                return windowPointer->WindowProcedure(hwnd, msg, wParam, lParam);
-            };
-
-            return DefWindowProcW(hwnd, msg, wParam, lParam);
-        };
-
-
         // Create the main window
         WNDCLASSEXW windowClass = { 0 };
         windowClass.cbSize = sizeof(WNDCLASSEXW);
@@ -138,7 +197,7 @@ private:
         windowClass.hInstance = _hInstance;
         windowClass.lpszClassName = _windowClassName.c_str();
 
-        windowClass.lpfnWndProc = tempWindowProc;
+        windowClass.lpfnWndProc = WinProcDispatch;
 
         windowClass.hCursor = LoadCursorW(NULL, IDC_ARROW);
         windowClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
