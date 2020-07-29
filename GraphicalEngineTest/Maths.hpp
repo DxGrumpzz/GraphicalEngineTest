a816d166c02b6f2f614d6a8cbdb5a07a950f5c20
Josef Lintz <yosi1.lints@gmail.com>
2020-07-29 17:36:53 +0300

    * "Slightly" improved rendering

--- GraphicalEngineTest/Maths.hpp
+++ GraphicalEngineTest/Maths.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdint>
+
 namespace Maths
 {
     inline constexpr float PI = 3.14159265358;
@@ -9,11 +11,18 @@ namespace Maths
     {
         return degrees * (PI / 180);
     };
-    
+
     float RadiansToDegrees(float radians)
     {
-        return radians* (180 / PI);
+        return radians * (180 / PI);
     };
 
+
+    std::uint64_t Cartesian2DTo1D(int x, int y, int length)
+    {
+        std::uint64_t index = static_cast<std::uint64_t>(x) + static_cast<std::uint64_t>(length) * static_cast<std::uint64_t>(y);
+
+        return index;
+    };
 
 }
--- GraphicalEngineTest/TestScene.hpp
+++ GraphicalEngineTest/TestScene.hpp
@@ -26,7 +26,7 @@ private:
     int _mapWidth = 10;
     int _mapHeight = 10;
 
-    float _playerFOV = 2.0f;//Maths::PI / 4;
+    float _playerFOV = Maths::DegreesToRadians(70);
 
     float _maxDepth = 10;
 
@@ -79,21 +79,13 @@ public:
 
         if (_window.GetKeyboard().GetKeyState('W') == KeyState::Held)
         {
-            _playerY -= 0.5 * deltaTime;
+            _playerY += std::sinf(_playerLookAtAngle) * 2.0f * deltaTime;
+            _playerX += std::cosf(_playerLookAtAngle) * 2.0f * deltaTime;
         }
         else if (_window.GetKeyboard().GetKeyState('S') == KeyState::Held)
         {
-            _playerY += 0.5 * deltaTime;
-        };
-
-
-        if (_window.GetKeyboard().GetKeyState('A') == KeyState::Held)
-        {
-            _playerX -= 0.5 * deltaTime;
-        }
-        else if (_window.GetKeyboard().GetKeyState('D') == KeyState::Held)
-        {
-            _playerX += 0.5 * deltaTime;
+            _playerY -= std::sinf(_playerLookAtAngle) * 2.0f * deltaTime;
+            _playerX -= std::cosf(_playerLookAtAngle) * 2.0f * deltaTime;
         };
 
     };
@@ -103,15 +95,14 @@ public:
     {
         for (int x = 0; x < _window.GetWindowWidth(); x++)
         {
-            float rayAngle = (_playerLookAtAngle - _playerFOV / 2.0f) + (static_cast<float>(_playerX) / static_cast<float>(_window.GetWindowWidth())) * _playerFOV;
-
+            float rayAngle = (_playerLookAtAngle - _playerFOV / 2.0f) + (static_cast<float>(x) / static_cast<float>(_window.GetWindowWidth())) * _playerFOV;
 
             float distanceToWall = 0.0f;
 
             bool hitWall = false;
 
-            float playerEyeX = std::cos(_playerLookAtAngle);
-            float playerEyeY = std::sin(_playerLookAtAngle);
+            float playerEyeX = std::cosf(rayAngle);
+            float playerEyeY = std::sinf(rayAngle);
 
             while ((hitWall == false) &&
                    distanceToWall < _maxDepth)
@@ -130,12 +121,10 @@ public:
                 }
                 else
                 {
-
                     if (_map[testX + _mapWidth * testY] == true)
                     {
                         hitWall = true;
                     };
-
                 };
             };
 
@@ -143,6 +132,7 @@ public:
             int ceiling = static_cast<float>(_window.GetWindowHeight() / 2.0) - _window.GetWindowHeight() / distanceToWall;
             int floor = _window.GetWindowHeight() - ceiling;
 
+            // Calculate wall shading based on distance
             std::uint8_t shade = 0;
 
             if (distanceToWall <= _maxDepth / 4.0f)
@@ -156,6 +146,7 @@ public:
             else
                 shade = 190;
 
+            // Draw the frame column-by-column
             for (int y = 0; y < _window.GetWindowHeight(); y++)
             {
                 if (y < ceiling)
@@ -181,8 +172,11 @@ public:
 
         };
 
+        // Draw a little minimap showing where the player is located at
         DrawMiniMap();
     };
+
+
 
     void DrawMiniMap()
     {
@@ -190,11 +184,13 @@ public:
         int miniMapYOffset = 0;
 
 
-        int miniMapWidthScale = 8;
-        int miniMapHeightScale = 8;
+        int miniMapWidthScale = 10;
+        int miniMapHeightScale = 10;
+
+        float lineLength = ((miniMapWidthScale + miniMapHeightScale) / 2) + 5.0f;
 
 
-        // Draw the map
+        // Draw the _map
         for (std::uint64_t x = 0; x < _mapWidth * miniMapWidthScale; x++)
         {
             for (std::uint64_t y = 0; y < _mapHeight * miniMapHeightScale; y++)
@@ -204,25 +200,26 @@ public:
         };
 
 
-        // Draw map blocks
-        for (std::uint64_t x = 0; x < _mapWidth; x++)
+        // Draw _map blocks
+        for (std::uint64_t a = 0; a < _mapHeight * _mapWidth; a++)
         {
-            for (std::uint64_t y = 0; y < _mapHeight; y++)
+            if (_map[a] == true)
             {
+                // Find the block's X and Y
+                int mapBlockX = a % _mapWidth;
+                int mapBlockY = a / _mapWidth;
 
-                if (_map[x + _mapWidth * y] == true)
+
+                for (std::uint64_t y = 0; y < miniMapHeightScale; y++)
                 {
-                    for (std::uint64_t _x = 0; _x < miniMapWidthScale; _x++)
+                    for (std::uint64_t x = 0; x < miniMapWidthScale; x++)
                     {
-                        for (std::uint64_t _y = 0; _y < miniMapHeightScale; _y++)
-                        {
-                            _graphics.DrawPixel(_x + (x * miniMapWidthScale) + miniMapXOffset,
-                                                _y + (y * miniMapHeightScale) + miniMapYOffset,
-                                                Colours::Black, false);
-                        };
+                        _graphics.DrawPixel(x + (mapBlockX * miniMapWidthScale) + miniMapXOffset,
+                                            y + (mapBlockY * miniMapHeightScale) + miniMapYOffset,
+                                            Colours::Black, false);
+
                     };
                 };
-
             };
         };
 
@@ -243,7 +240,6 @@ public:
         float angleX = std::cosf(_playerLookAtAngle);
         float angleY = std::sinf(_playerLookAtAngle);
 
-        float lineLength = 10.0f;
 
         // Draw a line starting from the player's position to the view ray's angle
         Vector2D p0(miniMapXOffset + ((_playerX * miniMapWidthScale) + miniMapWidthScale / 2),
@@ -253,10 +249,10 @@ public:
         p1.X += (angleX * lineLength);
         p1.Y += (angleY * lineLength);
 
-        _graphics.DrawLine(p0, p1, Colours::Black, false);
-        _graphics.DrawLine({ p0.X, p0.Y + 1 }, { p1.X, p1.Y + 1 }, Colours::Black, false);
+        _graphics.DrawLine(p0, p1, Colours::Red, false);
+        _graphics.DrawLine({ p0.X, p0.Y + 1 }, { p1.X, p1.Y + 1 }, Colours::Red, false);
 
-        _graphics.DrawLine({ p0.X + 1, p0.Y }, { p1.X + 1, p1.Y }, Colours::Black, false);
+        _graphics.DrawLine({ p0.X + 1, p0.Y }, { p1.X + 1, p1.Y }, Colours::Red, false);
     };
 
 };
